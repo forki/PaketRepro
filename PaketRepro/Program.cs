fa5b5998a2b865d5552ad62a021f9f1b55826a2e
Manuel Grundner <m.grundner@paragraph-software.at>
2015-08-24 13:27:11 +0200

    init commit

---
+++ PaketRepro/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaketRepro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        private async Task DoAsync()
+        {
+            await TaskEx.FromResult(1);
+        }
+    }
+}
